122c4d212e4c21a3145d873a213a11860a0a1131
Nutnicha-S <s6101012630089@email.kmutnb.ac.th>
2021-10-27 20:54:03 +0700

    Show ISCO-88

--- jobportal/models.py
+++ jobportal/models.py
@@ -2,13 +2,13 @@ from djongo import models
 
 # Create your models here.
 class ISCO(models.Model):
-    id = models.ObjectIdField()
+    _id = models.ObjectIdField()
     Group = models.TextField()
     Name = models.TextField()
     Description = models.TextField()
 
 class JobAnalysis(models.Model):
-    id = models.ObjectIdField()
+    _id = models.ObjectIdField()
     Career = models.TextField()
     HardSkill = models.TextField()
     SoftSkill = models.TextField()
--- jobportal/views.py
+++ jobportal/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import ISCO, JobAnalysis
 
 # Create your views here.
+def Occupation(request):
+    data = ISCO.objects.all()
+    return render(request, 'Occupation.html', {'Occupation':data})
